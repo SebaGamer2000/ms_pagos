c352472867de1ecb5307bb6c6ae9456e7a8a7e59
rbonzi <benjaminosorio902@gmail.com>
2026-05-13 02:12:56 -0400

    Agregado GetMapping (Listar, listar por id y filtrar todos los pagos x run)

--- src/main/java/com/Pagos/Pagos/model/Pago.java
+++ src/main/java/com/Pagos/Pagos/model/Pago.java
@@ -1,4 +1,30 @@
 package com.Pagos.Pagos.model;
 
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "pagos")
+
 public class Pago {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPago;
+
+    @Column(nullable = false, length = 9)
+    private String runPagado;
+
+    @Column(nullable = false)
+    private Long montoPagado;
+
+    @Column(nullable = false)
+    private Long membresiaPagada;
+
 }
