582e073d0ca027aed21528534809588a2e135fe1
Seba <seb.santis@duocuc.cl>
2026-06-19 00:37:41 -0400

    Arreglo de errores

--- src/main/java/com/Pagos/Pagos/config/WebClientConfiguration.java
+++ src/main/java/com/Pagos/Pagos/config/WebClientConfiguration.java
@@ -1,15 +1,20 @@
 package com.Pagos.Pagos.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.client.WebClient;
+import java.util.Base64;
 
 @Configuration
 public class WebClientConfiguration {
-    @LoadBalanced
+
     @Bean
-    public WebClient.Builder webClientBuilder(){
-        return WebClient.builder();
+    public WebClient.Builder webClientBuilder() {
+        String credentials = Base64.getEncoder()
+                .encodeToString("admin:admin123".getBytes());
+
+        return WebClient.builder()
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Basic " + credentials);
     }
 }
